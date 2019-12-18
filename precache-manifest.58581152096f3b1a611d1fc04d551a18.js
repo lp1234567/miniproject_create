82d3d47b92d69d5358f4dbea2c14065a2102f73b
lp1234567 <45986110+lp1234567@users.noreply.github.com>
2019-12-18 14:14:39 +0800

    打包

---
+++ precache-manifest.58581152096f3b1a611d1fc04d551a18.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba74e3c1867d610e73be8fc1ad385080",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9922dd7afbc0b5e77678",
+    "url": "/static/css/2.05031517.chunk.css"
+  },
+  {
+    "revision": "c89b0748ca5f4075c76d",
+    "url": "/static/css/4.3f88da4e.chunk.css"
+  },
+  {
+    "revision": "ffeffc2903becc432045",
+    "url": "/static/css/main.24f55a29.chunk.css"
+  },
+  {
+    "revision": "9922dd7afbc0b5e77678",
+    "url": "/static/js/2.cf4e05d7.chunk.js"
+  },
+  {
+    "revision": "1a8241271e97990bd626",
+    "url": "/static/js/3.15ca1284.chunk.js"
+  },
+  {
+    "revision": "c89b0748ca5f4075c76d",
+    "url": "/static/js/4.8f0e0387.chunk.js"
+  },
+  {
+    "revision": "ffeffc2903becc432045",
+    "url": "/static/js/main.1a285e1e.chunk.js"
+  },
+  {
+    "revision": "a08d388165f92f6a3ba3",
+    "url": "/static/js/runtime~main.2f3281fd.js"
+  }
+]);
